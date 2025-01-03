ca0b9c307097651c11f1a3232cc22741059d86d2
Gladys Ange Isingizwe <gladyisingizwe@gmail.com>
2025-01-03 12:16:32 -0800

    implemented todo dao

--- src/main/java/com/teamtreehouse/techdegrees/dao/Sql2oTodoDao.java
+++ src/main/java/com/teamtreehouse/techdegrees/dao/Sql2oTodoDao.java
@@ -4,6 +4,7 @@ import com.teamtreehouse.techdegrees.exc.DaoException;
 import com.teamtreehouse.techdegrees.model.Todo;
 import org.sql2o.Connection;
 import org.sql2o.Sql2o;
+import org.sql2o.Sql2oException;
 
 import java.util.List;
 
@@ -13,30 +14,62 @@ public class Sql2oTodoDao implements TodoDao{
 
     public Sql2oTodoDao(Sql2o sql2o) {
         this.sql2o = sql2o;
-
-        //  Create todos table if it doesn't exist
-        try (Connection conn = sql2o.open()) {
-
-        }
     }
 
     @Override
     public void create(Todo todo) throws DaoException {
-
-    }
-
-    @Override
-    public void update(int id, Todo todo) {
-
-    }
-
-    @Override
-    public void delete(int id) {
-
+        String sql = "INSERT INTO todos(name, isCompleted) VALUES (:name, :isCompleted)";
+        try (Connection con = sql2o.open()) {
+            int id = (int) con.createQuery(sql, true) // `true` retrieves generated keys
+                    .bind(todo)
+                    .executeUpdate()
+                    .getKey();
+            todo.setId(id); // Set tge generated ID back into the `Todo` object
+        } catch (Sql2oException ex) {
+            throw new DaoException(ex, "Problem creating todo");
+        }
     }
 
     @Override
     public List<Todo> findAll() {
-        return List.of();
+        try (Connection con = sql2o.open()) {
+            return con.createQuery("SELECT * FROM todos")
+                    .executeAndFetch(Todo.class);
+        }
+    }
+
+    @Override
+    public Todo findById(int id) {
+        try (Connection con = sql2o.open()) {
+            return con.createQuery("SELECT * FROM todos WHERE id = :id")
+                    .addParameter("id", id)
+                    .executeAndFetchFirst(Todo.class);
+        }
+    }
+
+    @Override
+    public void update(int id, Todo todo) throws DaoException {
+        String sql = "UPDATE todos SET name = :name, isCompleted = :isCompleted WHERE id = :id";
+        try (Connection con = sql2o.open()) {
+            con.createQuery(sql)
+                    .addParameter("id", id)
+                    .addParameter("name", todo.getName())
+                    .addParameter("isCompleted", todo.isCompleted())
+                    .executeUpdate();
+        } catch (Sql2oException ex) {
+            throw new DaoException(ex, "Problem updating todo.");
+        }
+    }
+
+    @Override
+    public void delete(int id) throws DaoException {
+        String sql = "DELETE FROM todos WHERE id = :id";
+        try (Connection con = sql2o.open()) {
+            con.createQuery(sql)
+                    .addParameter("id", id)
+                    .executeUpdate();
+        } catch (Sql2oException ex) {
+            throw new DaoException(ex, "Problem deleting todo.");
+        }
     }
 }
