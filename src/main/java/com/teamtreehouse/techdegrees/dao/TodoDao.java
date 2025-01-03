0e0973cf84818ea84c222f93e471c4cd65f67eac
Gladys Ange Isingizwe <gladyisingizwe@gmail.com>
2025-01-03 12:03:28 -0800

    added findById method to the interface

--- src/main/java/com/teamtreehouse/techdegrees/dao/TodoDao.java
+++ src/main/java/com/teamtreehouse/techdegrees/dao/TodoDao.java
@@ -8,10 +8,13 @@ import java.util.List;
 public interface TodoDao {
     void create(Todo todo) throws DaoException; // Add a new Todo
 
-    void update(int id, Todo todo); // Update an existing Todo
-
-    void delete(int id); // Delete a Todo by Id
-
     List<Todo> findAll(); // Retrieve all Todos
+
+    Todo findById(int id);
+
+    void update(int id, Todo todo) throws DaoException; // Update an existing Todo
+
+    void delete(int id) throws DaoException; // Delete a Todo by Id
+
 
 }
