f09b9401ed9057c556a149851f4f7e5c57009493
Gladys Ange Isingizwe <gladyisingizwe@gmail.com>
2025-01-06 10:39:17 -0800

    modified database

--- src/main/resources/db/init.sql
+++ src/main/resources/db/init.sql
@@ -1,7 +1,7 @@
 -- Create the todos table if it doesn't already exist
 
 CREATE TABLE IF NOT EXISTS todos (
-    id INTEGER PRIMARY KEY auto_increment, -- Auto-incrementing integer ID for each todo
-    name VARCHAR, -- Name or Title of the todo item
-    isCompleted BOOLEAN -- Status indicating if the todo is completed (true/false)
+    id INT AUTO_INCREMENT PRIMARY KEY, -- Auto-incrementing integer ID for each todo
+    name VARCHAR(255) NOT NULL, -- Name or Title of the todo item
+    isCompleted BOOLEAN NOT NULL -- Status indicating if the todo is completed (true/false)
 );
